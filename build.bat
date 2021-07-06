2ce9fc5d992ee1383fbe0097f93ca19fbfd80acf
chinoll <chinoll@protonmail.com>
2021-07-06 18:11:41 +0800

    update build.bat

--- build.bat
+++ build.bat
@@ -1,10 +1,10 @@
 call gradlew.bat build
-rm /s/q build_jar_tmp
+del /s/q build_jar_tmp
 mkdir build_jar_tmp
 copy luaj-jse-3.0.2.jar build_jar_tmp
 copy build/libs/codemagic-0.0.1.jar build_jar_tmp
 cd build_jar_tmp
 jar xvf codemagic-0.0.1.jar
 jar xvf luaj-jse-3.0.2.jar
-rm /q *.jar
+del /q *.jar
 jar cvfM codemagic.jar .
